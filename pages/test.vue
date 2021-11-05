ea6ff959dc9071000a7d1104f6484cfb10c40848
weisiqian <1535658549@qq.com>
2021-11-05 16:25:35 +0800

    issue

---
+++ pages/test.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    test page
+    <div @click="handleNavigate">click</div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { defineComponent } from 'vue'
+import { useNuxtApp } from '#app';
+
+const handleNavigate = () => {
+  try {
+    const nuxtApp = useNuxtApp();
+    alert(nuxtApp)
+    console.error('nuxtApp', nuxtApp)
+  } catch(e){
+    console.error(e)
+    alert(e)
+  }
+}
+</script>
