2fac36b4490c18235de6eb433d28c253ebf28ac5
DESKTOP-4BBQH70\Admin <1535658549@qq.com>
2021-11-09 22:16:19 +0800

    decorator error

--- pages/1.vue
+++ pages/1.vue
@@ -1,16 +1,34 @@
 <template>
   <div>
     page 1
-    <div @click="handleNavigate">click</div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { navigateTo as navigateTo1 } from '@/utils/common';
-import { navigateTo as navigateTo2 } from '@/composables/common';
+import { Post } from '@/entity/post';
+// import {
+//   validate,
+//   validateOrReject
+// } from 'class-validator';
 
-const handleNavigate = () => {
-  // navigateTo1('/2')
-  navigateTo2('/2')
-}
+let post = new Post();
+post.title = 'Hello'; // should not pass
+post.text = 'this is a great post about hell world'; // should not pass
+post.rating = 11; // should not pass
+post.email = 'google.com'; // should not pass
+post.site = 'googlecom'; // should not pass
+
+// validate(post).then(errors => {
+//   // errors is an array of validation errors
+//   if (errors.length > 0) {
+//     console.log('validation failed. errors: ', errors);
+//   } else {
+//     console.log('validation succeed');
+//   }
+// });
+
+// validateOrReject(post).catch(errors => {
+//   console.log('Promise rejected (validation failed). Errors: ', errors);
+// });
+
 </script>
